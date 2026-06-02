abb197e2edb1d7f7002548ecf25fabe50fd054fc
SocketSomeone <socket.someone@gmail.com>
2026-06-02 04:37:15 +0300

    chore: update zsh and git config

--- links/git.ps1
+++ links/git.ps1
@@ -1,5 +1,5 @@
 Write-Info "Adding symlink for Git configuration files"
 Add-Symlink "$($env:USERPROFILE)\.gitconfig" "$($CONFIG)\git\.gitconfig"
-# git config --global core.excludesfile $env:USERPROFILE\.gitignore
-Add-Symlink "$($env:USERPROFILE)\.gitignore" "$($CONFIG)\git\.gitignore"
+# git config --global core.excludesfile $env:USERPROFILE\.gitignore_global
+Add-Symlink "$($env:USERPROFILE)\.gitignore_global" "$($CONFIG)\git\.gitignore_global"
 Write-Success "Git configuration symlink added successfully"
