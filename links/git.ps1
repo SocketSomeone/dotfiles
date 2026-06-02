abdfe0c8802fe05b01b0c959e0675e357aced0e9
SocketSomeone <socket.someone@gmail.com>
2026-06-02 04:17:48 +0300

    chore: update gitconfig

--- links/git.ps1
+++ links/git.ps1
@@ -1,4 +1,5 @@
 Write-Info "Adding symlink for Git configuration files"
 Add-Symlink "$($env:USERPROFILE)\.gitconfig" "$($CONFIG)\git\.gitconfig"
+# git config --global core.excludesfile $env:USERPROFILE\.gitignore
 Add-Symlink "$($env:USERPROFILE)\.gitignore" "$($CONFIG)\git\.gitignore"
 Write-Success "Git configuration symlink added successfully"
