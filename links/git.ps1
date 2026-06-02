55b34ab017ce632e7a73901ea9206d2e58fe51e8
SocketSomeone <socket.someone@gmail.com>
2026-06-02 04:14:10 +0300

    chore: add gitignore global

--- links/git.ps1
+++ links/git.ps1
@@ -1,3 +1,4 @@
 Write-Info "Adding symlink for Git configuration files"
 Add-Symlink "$($env:USERPROFILE)\.gitconfig" "$($CONFIG)\git\.gitconfig"
+Add-Symlink "$($env:USERPROFILE)\.gitignore" "$($CONFIG)\git\.gitignore"
 Write-Success "Git configuration symlink added successfully"
