01cf01898e6cfc6c523bedb92f90d34c05958799
SocketSomeone <socket.someone@gmail.com>
2026-06-09 20:54:34 +0300

    fix: add some macos specific changes

--- setup/os.sh
+++ setup/os.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# Some macOS-specific defaults and tweaks. Run this after installing packages with Homebrew.
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  echo "Configuring macOS with sane defaults…"
+  echo
+else
+  exit 0
+fi
